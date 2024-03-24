c27275808299448961309174f87bf0d796001831
Hussain <sidmohd0404@gmail.com>
2024-03-24 08:04:52 +0530

    Testing Done Cleaning

--- src/main/java/com/capstone/CapstoneApplication.java
+++ src/main/java/com/capstone/CapstoneApplication.java
@@ -9,14 +9,7 @@ public class CapstoneApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneApplication.class, args);
 		System.out.println("Working");
-		System.out.println("practice");
-		System.out.println("feature");
-		System.out.println("Its making more sense");
-		System.out.println("its part");
-		System.out.println("feature 2");
-		System.out.println("feature 3");
-		System.out.println("Feature 3 updating");
-		System.out.println("Taking pull from master nd making changes in feature");
+
 	}
 
 }
